d5b95420f8919a273eae8144a93f26b81fb667db
AndreiMocirean <99763178+AndreiMocirean@users.noreply.github.com>
2023-01-28 18:37:02 +0200

    test

---
+++ page.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>nokoSocial - Responsive Social Media Website Using HTML, CSS & JavaScript</title>
+    <!-- ICONSCOUT CDN -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v2.1.6/css/unicons.css">
+    <!-- STYLESHEET -->
+    <link rel="stylesheet" href="./style.css">
+    </head>
+    <body>
+    <div class="logins">
+                    <!------------------- FEED 1 --------------------->
+                    <div class="feed">
+                        <div class="search">
+                            <div class="user">
+                            <div class="search-bar">
+                        <i class="uil uil-search"></i>
+                        <input type="search" placeholder="Search messages" id="message-search">
+                    </div>
+                                <div class="profile-photo">
+                                    <img src="./images/profile-13.jpg">
+                                </div>
+                                
+
+</html>
